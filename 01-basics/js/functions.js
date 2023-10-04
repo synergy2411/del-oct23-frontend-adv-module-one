7b53996731351d76cbe34c3f0db34614fef40837
Sumit Khandelwal <synergy2411@outlook.com>
2023-10-04 14:49:56 +0530

    this keyword

--- 01-basics/js/functions.js
+++ 01-basics/js/functions.js
@@ -53,6 +53,9 @@
 
 // const xyz = testX();
 // console.log(xyz())          // 102
+// console.log(xyz())          // 103
+// console.log(xyz())          // 104
+// console.log(xyz())          // 105
 
 
 // function buildTicket(transport) {
@@ -95,35 +98,105 @@
 
 
 
-// HOF || Callback
+// HOF || CALLBACKS
+// ----------------
 // setTimeout(() => { console.log("2 seconds left") }, 2000);
 
 
-function greet(err, username) {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log("Guter Morgan isle :", username);
-}
+// function greet(err, username) {
+//     if (err) {
+//         console.error(err);
+//         return;
+//     }
+//     console.log("Guter Morgan isle :", username);
+// }
 
-function demoFn(arr, cb) {
-    if (arr.length > 2) {
-        cb(null, "John Doe");
-    } else {
-        cb(new Error("Too low values"))
-    }
-    console.log("Demo Function executed")
-}
-
-
-demoFn([1, 2], greet);
+// function demoFn(arr, cb) {
+//     if (arr.length > 2) {
+//         cb(null, "John Doe");
+//     } else {
+//         cb(new Error("Too low values"))
+//     }
+//     console.log("Demo Function executed")
+// }
 
 
-let numbers = [101, 103]
+// demoFn([1, 2, 3], greet);
 
-function xyz(value, index, array) {
-    console.log(this);
-}
 
-numbers.map(xyz)
+
+
+// function multiply(n1, n2) {
+//     return n1 * n2;
+// }
+
+// console.log(multiply(2, 3))
+
+
+
+
+// PARTIAL / CURRYING
+
+// mul(5,3,2) || mul(5)(3)(2)
+
+// function mul(n1) {
+//     return function (n2) {
+//         return function (n3) {
+//             return n1 * n2 * n3;
+//         }
+//     }
+// }
+
+// const multiplyBy5 = mul(5)
+
+// const myltiplyBy15 = multiplyBy5(3)
+
+// console.log(myltiplyBy15(2))
+
+
+// console.log(mul(10)(5)(3));
+
+// console.log(mul(5)(2))
+
+
+
+
+
+// let x = 301;
+
+// function A() {
+//     let x = 201;
+//     function B() {
+//         // let x = 101;
+//         console.log(x)          // 101
+//     }
+//     B();
+// }
+
+// A();
+
+
+// LEXICAL SCOPE || SCOPE CHAINING || VARIABLE LOOKUP
+
+// let x = 101;
+
+// function B(){
+//     console.log(x);         // ?
+// }
+
+// function A(){
+//     let x = 99;
+//     B()
+// }
+
+// A()
+
+
+
+
+// IIFE
+(function () {
+    var x = 201;
+    console.log("IIFE - Immediately Invoked Function Expression")
+})();
+
---
+++ 01-basics/js/this.js
@@ -0,0 +1,101 @@
+// Method
+
+// function greet() {
+//     return `Hola ${this.firstName} ${this.lastName}`;
+// }
+
+// let user = {
+//     firstName: "Ross",
+//     lastName: "Green",
+// }
+
+// const boundedOne = greet.bind(user)
+// console.log(boundedOne())
+
+// let userTwo = {
+//     firstName: "Rachel",
+//     lastName: "Gellar"
+// }
+
+// const boundedTwo = greet.bind(userTwo);
+
+// console.log(boundedTwo())
+
+
+// function greet(username, age) {
+//     return `
+//     Greetings Mr. ${username},
+//     You are ${age} years old!
+//     Email : ${this.email}
+//     `;
+// }
+
+
+// let userOne = {
+//     email: "userOne@test"
+// }
+
+// console.log(greet.call(userOne, "John", 32));
+
+// console.log(greet.apply(userOne, ["Jenny", 28]))
+
+
+// Function
+
+// function demoThis() {
+//     console.log(this);          // window object
+// }
+
+// demoThis();
+
+
+
+// Event Listeners
+
+// window.onload = function () {
+
+//     const btnSave = document.getElementById("btnSave");
+
+//     const txtMessage = document.getElementById("txtMessage");
+
+//     txtMessage.addEventListener("input", function () {
+//         console.log(this);      // ?
+//     })
+
+//     btnSave.addEventListener("click", function (event) {
+//         event.preventDefault();
+
+//         console.log(this);     // ?
+//     })
+
+// }
+
+
+
+
+// Arrow Function
+
+
+let user = {
+    email: "test@test",
+    age: 32,
+    getDetails: function () {
+        return () => this.email + " - " + this.age
+    }
+
+}
+
+
+const nestedFn = user.getDetails()
+
+console.log(nestedFn());
+
+
+
+
+
+
+
+
+
+// Call Apply Bind
