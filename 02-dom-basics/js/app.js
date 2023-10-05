cf48449c5b1763fb47019541936b9b7dedcdb3a5
Sumit Khandelwal <synergy2411@outlook.com>
2023-10-05 12:11:43 +0530

    budget app completed

--- 02-dom-basics/js/app.js
+++ 02-dom-basics/js/app.js
@@ -5,10 +5,55 @@ window.onload = function () {
     let txtTitle = document.getElementById("title");
     let txtAmount = document.getElementById("amount");
     let txtDate = document.getElementById("date");
+    let expenseContainer = document.getElementById("expense-container");
+
+    let expenseList = [];
+
+    const deleteButtonRegistration = () => {
+        const btnDanger = document.querySelectorAll(".btn-danger");
+        btnDanger.forEach(btn => {
+            btn.addEventListener("click", function (e) {
+                e.preventDefault();
+                const listItem = this.closest(".list-group-item");
+                listItem.remove()
+            })
+        })
+    }
+
+    const render = () => {
+        expenseContainer.innerHTML = '';
+        expenseList.forEach(expense => {
+            const liElement = document.createElement("li");
+            const btnDelete = document.createElement("button");
+            btnDelete.innerText = "✖";
+            btnDelete.classList.add("btn", "btn-danger", "btn-sm")
+
+            liElement.classList.add("list-group-item", "d-flex", "justify-content-space-between", "mb-3");
+            liElement.innerHTML = `
+                <p class='lead'> You have spend <strong>INR${expense.amount}</strong> on ${expense.title} dated ${expense.date}
+            `
+            liElement.setAttribute("data-id", expense.id);
+            liElement.appendChild(btnDelete);
+            expenseContainer.appendChild(liElement);
+            deleteButtonRegistration()
+        });
+    }
 
     btnSave.addEventListener("click", function (event) {
         event.preventDefault();
-        console.log(txtTitle.value, txtAmount.value, txtDate.value)
+        if (txtTitle.value.trim() === "" || txtAmount.value.trim() === "" || txtDate.value.trim() === "") {
+            return;
+        }
+        expenseList.push({
+            id: Math.round(Math.random() * 1000).toString(),
+            title: txtTitle.value.toUpperCase(),
+            amount: Number(txtAmount.value),
+            date: new Date(txtDate.value)
+        })
+        txtTitle.value = ""
+        txtAmount.value = ""
+        txtDate.value = ""
+        render();
     })
 
 
@@ -19,3 +64,6 @@ window.onload = function () {
         txtDate.value = "";
     })
 }
+
+// You spend ${AMOUNT} on DATED ${DATE} for ${TITLE} - <DELETE_BUTTON>
+
